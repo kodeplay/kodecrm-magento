9f65c659c7fe4c514f5b6df02e45e572e952c9bc
Vineet Naik <vineet.naik@kodeplay.com>
2012-07-31 15:29:23 +0530

    Fix whitespace and indentation

--- magento/app/code/local/Kodecrm/Feed/controllers/IndexController.php
+++ magento/app/code/local/Kodecrm/Feed/controllers/IndexController.php
@@ -1,35 +1,35 @@
 <?php
 class Kodecrm_Feed_IndexController extends Mage_Core_Controller_Front_Action
 {
-	public function indexAction ()
-	{
+    public function indexAction ()
+    {
 
-		if(Mage::getStoreConfig('chat_section/feed_group/status')) {
+        if(Mage::getStoreConfig('chat_section/feed_group/status')) {
 				
-			$collection = Mage::getModel('catalog/product')
-			->getCollection()
-			->addAttributeToSelect('*');
+            $collection = Mage::getModel('catalog/product')
+                ->getCollection()
+                ->addAttributeToSelect('*');
 
-			$store = Mage::app()->getStore();
+            $store = Mage::app()->getStore();
 
-			$feedarr = array();
-			$feedarr['title'] = $store->getName();
-			$feedarr['link']  = $store->getUrl();
-			$feedarr['item']  = array();
+            $feedarr = array();
+            $feedarr['title'] = $store->getName();
+            $feedarr['link']  = $store->getUrl();
+            $feedarr['item']  = array();
 
-			$currency_code = Mage::app()->getStore()->getCurrentCurrencyCode();
+            $currency_code = Mage::app()->getStore()->getCurrentCurrencyCode();
 
-			foreach ($collection as $product) {
+            foreach ($collection as $product) {
 					
-				$categories = array();
-				$cat_arr = $product->getCategoryCollection()->exportToArray();
+                $categories = array();
+                $cat_arr = $product->getCategoryCollection()->exportToArray();
 					
-				foreach($cat_arr as $category) {
-					$categories[] = Mage::getModel('catalog/category')
-					->load($category['entity_id'])->getName();
-				}
+                foreach($cat_arr as $category) {
+                    $categories[] = Mage::getModel('catalog/category')
+                        ->load($category['entity_id'])->getName();
+                }
 					
-				$feedarr['item'][] = array(
+                $feedarr['item'][] = array(
                     'title' => $product->getName(),
                     'brand' => $product->getAttributeText('manufacturer'),
                     'description' => $product->getDescription(),
@@ -40,18 +40,18 @@ class Kodecrm_Feed_IndexController extends Mage_Core_Controller_Front_Action
                     'currency' => $currency,
                     'availability' => (int) $product->isInStock(),
                     'category' => $categories
-				);
-			}
+                );
+            }
 
-			$feed_helper = Mage::helper('feed');
+            $feed_helper = Mage::helper('feed');
 
-                        header('Content-Type: application/rss+xml; charset=utf-8');
+            header('Content-Type: application/rss+xml; charset=utf-8');
 
-			$feed = $feed_helper->kodecrm_feed_create($feedarr);
+            $feed = $feed_helper->kodecrm_feed_create($feedarr);
 
-			echo $feed;
-			exit();
-		}
+            echo $feed;
+            exit();
+        }
 
-	}
+    }
 }
